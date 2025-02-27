77767b175b03550038dd8b193bc2029268a81687
Allen Robel <arobel@cisco.com>
2025-02-27 12:21:05 -1000

    Rework FastApi @app.on_event() startup handling
    FastAPI has deprecated @app.on_event().  The below changes implement the recommended replacement startup / shutdown handlers.
    
    1. app/app.py
    
    Modify app entry to align with current FastAPI recommended startup/shutdown handling.
    
    2. app/main.py
    
    - Remove startup code.
    - main.py is now essentially an importer for the endpoints used by the app.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .db import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(runner: FastAPI):
+    """
+    App entry point.
+
+    Place startup code before the yield statement.
+    Place cleanup code after the yield statement.
+    """
+    print(f"app version {runner.version}. Creating db and tables")
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)

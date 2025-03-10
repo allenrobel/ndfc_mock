8b675cde7c1b89021afcf4692a90cc4180fa9cd1
Allen Robel <arobel@cisco.com>
2025-03-10 12:50:06 -1000

    Remove v1_switch_delete() for now.
    1. app/v1/endpoints/inventory.py
    
    - v1_endpoint_delete(): Remove until this is implemented.  It was messing up the /docs page.

--- app/v1/endpoints/inventory.py
+++ app/v1/endpoints/inventory.py
@@ -266,35 +266,6 @@ def build_success_response():
     return copy.deepcopy(response)
 
 
-@app.delete("/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/{fabric_name}")
-def v1_delete_switch(*, session: Session = Depends(get_session), fabric_name: str):
-    """
-    # Summary
-
-    DELETE request handler
-
-    ## NDFC Response
-
-    {
-        "timestamp": 1739842602937,
-        "status": 404,
-        "error": "Not Found",
-        "path": "/rest/control/fabrics/f2"
-    }
-    """
-    db_fabric = session.exec(select(Fabric).where(Fabric.FABRIC_NAME == fabric_name)).first()
-    if not db_fabric:
-        detail = {}
-        detail["timestamp"] = int(datetime.datetime.now().timestamp())
-        detail["status"] = 404
-        detail["error"] = "Not Found"
-        detail["path"] = f"/rest/control/fabrics/{fabric_name}"
-        raise HTTPException(status_code=404, detail=detail)
-    session.delete(db_fabric)
-    session.commit()
-    return {f"Fabric '{fabric_name}' is deleted successfully!"}
-
-
 @app.get(
     "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/{fabric_name}/inventory/switchesByFabric",
     response_model=List[SwitchResponseModel],

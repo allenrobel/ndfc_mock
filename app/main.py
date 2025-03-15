9341bedc0be4adcffa922b6a08e6c9a3c756298c
Allen Robel <arobel@cisco.com>
2025-03-14 15:21:20 -1000

    inventory.rediscover: Add endpoint handler
    # Summary
    
    Add handler for the following NDFC endpoint:
    
    Verb: POST
    Path: `/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/{fabric_name}/inventory/rediscover/{serial_number}`
    
    ## Endpoint handler
    
    - v1/endpoints/lan_fabric/rest/control/fabrics/inventory/rediscover_post.py
    
    ## ./app/main.py
    
    - Update imports
    - app.include_router(rediscover_post.router, tags=["Inventory (v1)"])
    
    ## ./app/v1/models/inventory.py
    
    - Fix serialNumber parameter in SwitchDiscoverItem
    
    This was serial_number, but needs to be serialNumber

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .v1.endpoints.configtemplate.rest.config.templates import config_template_b
 from .v1.endpoints.fm.about import version_get
 from .v1.endpoints.fm.features import features_get
 from .v1.endpoints.lan_fabric.rest.control.fabrics import fabric_delete, fabric_get, fabric_post, fabric_put, fabrics_get
-from .v1.endpoints.lan_fabric.rest.control.fabrics.inventory import discover_post, internal_inventory_get, switches_by_fabric_get, test_reachability_post
+from .v1.endpoints.lan_fabric.rest.control.fabrics.inventory import discover_post, internal_inventory_get, rediscover_post, switches_by_fabric_get, test_reachability_post
 from .v1.endpoints.lan_fabric.rest.control.switches import fabric_name_get, overview
 from .v1.endpoints.lan_fabric.rest.control.switches.overview import v1_lan_fabric_rest_control_switches_overview_by_fabric_name
 from .v1.endpoints.lan_fabric.rest.control.switches.roles import roles_get, roles_post
@@ -27,6 +27,7 @@ app.include_router(switches_by_fabric_get.router, tags=["Inventory (v1)"])
 app.include_router(roles_get.router, tags=["Inventory (v1)"])
 app.include_router(roles_post.router, tags=["Inventory (v1)"])
 app.include_router(test_reachability_post.router, tags=["Inventory (v1)"])
+app.include_router(rediscover_post.router, tags=["Inventory (v1)"])
 app.include_router(fabric_name_get.router, tags=["Switches (v1)"])
 app.include_router(overview.router, tags=["Switches (v1)"])
 app.include_router(config_template_by_name.router, tags=["Templates (v1)"])

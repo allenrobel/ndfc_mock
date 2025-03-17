017c2ee8962ad0df9c5503e183497ceb9af6b381
Allen Robel <arobel@cisco.com>
2025-03-17 08:17:05 -1000

    fabrics.config-save POST: New endpoint
    1. app/main.py
    
    - Update imports
    - app.include_router(config_save_post.router, tags=["Fabrics (v1)"])
    
    2. app/v1/endpoints/lan_fabric/rest/control/fabrics/common.py
    
    - build_404_response()
      - New shared function
    
    3. app/v1/endpoints/lan_fabric/rest/control/fabrics/config_save_post.py
    
    - New endpoint handler
    - /appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/{fabric_name}/config-save

--- app/v1/endpoints/lan_fabric/rest/control/fabrics/common.py
+++ app/v1/endpoints/lan_fabric/rest/control/fabrics/common.py
@@ -1,4 +1,5 @@
 import copy
+import datetime
 
 
 def build_nv_pairs(fabric):
@@ -24,3 +25,28 @@ def build_response(fabric):
     response["id"] = fabric.id
     response["nvPairs"] = build_nv_pairs(fabric)
     return copy.deepcopy(response)
+
+
+def build_404_response(path: str) -> dict:
+    """
+    # Summary
+
+    Return an emulated NDFC 404 response which includes the request path.
+
+    ## Example Response
+
+    ```json
+    {
+        "timestamp": 1742232142,
+        "status": 404,
+        "error": "Not Found",
+        "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/F2/config-save"
+    }
+    ```
+    """
+    response = {}
+    response["timestamp"] = int(datetime.datetime.now().timestamp())
+    response["status"] = 404
+    response["error"] = "Not Found"
+    response["path"] = f"{path}"
+    return response
